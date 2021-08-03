5042c023a86d65729a0155e710c27972c2995b9b
Fashad Ahmed <fashad.ahmed20@gmail.com>
2021-08-03 12:46:30 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BMR from "./components/bmr";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <BMR />
+    </div>
+  );
+}
